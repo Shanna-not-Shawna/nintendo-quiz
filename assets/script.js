cc0b42c0909605bd5d1b82264ce8530d5df713c7
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-29 16:30:45 -0600

    Fixed bugs in timer and answer feedback

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@
 // User clicks Start to start quiz, (choose an answer, see feedback, go to next question) * 5, when all questions are answered OR time is up, the Game Over screen will appear and user will be asked to enter initials for the high score page. User then has the option to go back to start or erase high scores. 
 
 
-
 var startSection = document.getElementById("starter");
 var questionContainer = document.getElementById("questionSection");
 var formSection = document.getElementById("form");
@@ -11,6 +10,11 @@ var startButton = document.getElementById("start-btn");
 var countdownText = document.getElementById("countdown-text");
 var question = document.createElement("h2");
 var feedbackText = document.getElementById("feedback");
+var submitButton = document.getElementById("submit-btn");
+var timeLeft = 20;
+var timeInterval;
+var incorrectPenalty = 10;
+var createElP = document.createElement("p")
 
 // function startQuiz - hides start section, unhides question section which populates with 1st Q, starts timer
 startButton.addEventListener('click', startQuiz);
@@ -23,7 +27,7 @@ function startQuiz() {
     //show question container
     questionContainer.classList.remove('hide');
     formSection.classList.add('hide');
-    scoreBoard.classList.add('hide');
+    // scoreBoard.classList.add('hide');
     //show the 1st question
     displayQuestion();
 }
@@ -31,27 +35,21 @@ function startQuiz() {
 
 // add more time 
 function countdown() {
-    var timeLeft = 80;
-
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
 
         if (timeLeft > 1) {
             countdownText.textContent = timeLeft + ' seconds remaining';
             timeLeft--;
         } else {
-            countdownText.textContent = '';
-            // Use `clearInterval()` to stop the timer
-            clearInterval(timeInterval);
-            // endQuiz();
+            
+            
+            endQuiz();
         }
     }, 1000);
 }
 
-// function startTimer 
-// function setNextQuestion 
 // function selectAnswer use event.target buttons
 // form element wrapping an input for high score submit
-// function endQuiz
 
 var questionIndex = 0;
 var myQuestions = [{
@@ -78,6 +76,7 @@ var myQuestions = [{
 
 function displayQuestion() {
     questionContainer.innerHTML = "";
+    feedbackText.textContent = "";
     var currentQuestion = myQuestions[questionIndex];
     question.textContent = currentQuestion.questionText;
     questionContainer.append(question)
@@ -92,38 +91,82 @@ function displayQuestion() {
     }
 }
 
-function checkAnswer(isCorrect) {
-    console.log(isCorrect);
-      //increment questionIndex
-    questionIndex++;
-    //if there are more questions call display question
-        if(questionIndex < myQuestions.length) {
-            displayQuestion()
-            //if not call end quiz
-        } else {
-            endQuiz();
-        }
-}
-
 function endQuiz(){
+    countdownText.textContent = '';
+    feedbackText.textContent = "";
     //show the form
     formSection.classList.remove('hide');
     //hide the quiz section
     questionContainer.classList.add('hide');
-    //freeze and record timer
+    questionContainer.classList.remove('hide');
+    //freeze clock and store the time left on timer to be used for scoreboard
+    // Use `clearInterval()` to stop the timer
+    clearInterval(timeInterval);
 }
+
+function checkAnswer(isCorrect) {
+    console.log(isCorrect);
+    displayResult(isCorrect);  // Pass the correct value here
+    setTimeout(function () {
+        questionIndex++;
+        // if there are more questions call display question
+        if (questionIndex < myQuestions.length) {
+            displayQuestion();
+        // if not call end quiz
+        } else {
+            endQuiz();
+        }
+    }, 2000);
+}
+
+function displayResult(isCorrect) {
+    feedbackText.textContent = isCorrect ? "Correct!" : "Incorrect! -10 Seconds";
+    if (!isCorrect) {
+       timeLeft = Math.max(0, timeLeft - incorrectPenalty);
+    }
+}
+
+
+submitButton.addEventListener("click", saveScore);
 
 
 //this function is called whenever the button on the form is clicked
 function saveScore(){
     //save initials and score to local storage. Key initials, user entered string; key score, time remaining on timer.
-    var x = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = x;
-    
+    var initials = document.getElementById("myText").value;
+
+    if (!initials) return;
+
+    var topScores = JSON.parse(localStorage.getItem("topScores")) || [];
+
+    topScores.push({initials, timeLeft});
+
+    localStorage.setItem("topScores", JSON.stringify(topScores));
+
+    showScores();
+
+}
+
+// get object array from local storage, parse it, have an unordered list populated with a list item for each entry
+function showScores(){
+    var scoresHTML = "";
+
+    var topScores = JSON.parse(localStorage.getItem("topScores")) || [];
+    var scoresContainer = document.getElementById("scoresContainer");
+
+    for (const score of topScores) {
+        scoresHTML += `
+        <ul>
+        <li>${score.initials} ${score.timeLeft}</li>
+        </ul>
+        `
+    }
+    scoresContainer.innerHTML = scoresHTML;
+
 }
 
 document.addEventListener("click", function(event){
     if(event.target.className !== "answer-btn") return;
 
-    checkAnswer(event.target.dataset.correct);
+    checkAnswer(event.target.dataset.correct === "true");
 })
